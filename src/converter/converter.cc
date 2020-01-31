a4a9cb30a5bb29f634fa1f2805356c2065164266
Daniel Baud <danielbaudf@gmail.com>
2020-01-31 23:57:21 +0100

    added kmeans to get 8 base colors of image

--- src/converter/converter.cc
+++ src/converter/converter.cc
@@ -1,89 +1,117 @@
 #include "converter.hh"
 
 Converter::Converter(string path_in, string path_out)
-: in(path_in), out(path_out)
-{}
+: out(path_out)
+{
+    unsigned colors = 8;
+    srand(128);
+    this->image = fipImage();
+    this->image.load(path_in.c_str());
+    if (!this->image.isValid()) {
+        cerr << path_in << ": File is invalid" << endl;
+        exit(1);
+    }
+    unsigned width = this->image.getWidth();
+    unsigned height = this->image.getHeight();
+
+    this->clusters = vector<RGBQUAD>(colors);
+    
+    for (unsigned i = 0; i < colors; ++i) {
+        this->clusters[i].rgbRed = i < 4 ? 0 : 255;
+        this->clusters[i].rgbGreen = (i % 4) < 2 ? 0 : 255;
+        this->clusters[i].rgbBlue = i % 2 == 0 ? 0 : 255;
+    }
+
+    vector<unsigned> counts = vector<unsigned>(colors, 0);
+    vector<vector<double>> avgs = vector<vector<double>>(colors, vector<double>(3, 0));
+
+    for (unsigned x = 0; x < 10; ++x) {
+        for (unsigned i = 0; i < width; ++i) {
+            for (unsigned j = 0; j < height; ++j) {
+                RGBQUAD color;
+                this->image.getPixelColor(i, j, &color);
+                int cluster = this->get_cluster(color);
+                counts[cluster]++;
+                avgs[cluster][0] += color.rgbRed;
+                avgs[cluster][1] += color.rgbGreen;
+                avgs[cluster][2] += color.rgbBlue;
+            }
+        }
+        for (unsigned c = 0; c < colors; ++c) {
+            if (counts[c] > 0) {
+                this->clusters[c].rgbRed = (int)(avgs[c][0] / counts[c]);
+                this->clusters[c].rgbGreen = (int)(avgs[c][1] / counts[c]);
+                this->clusters[c].rgbBlue = (int)(avgs[c][2] / counts[c]);
+            }
+        }
+        counts = vector<unsigned>(colors, 0);
+        avgs = vector<vector<double>>(colors, vector<double>(3, 0));
+    }
+}
 
 bool Converter::convert() {
-    fipImage image_in = fipImage();
-
-    // Loads the image
-    image_in.load(this->in.c_str());
-    if (!image_in.isValid()) {
-        cerr << this->in << ": File is invalid" << endl;
-        return false;
-    }
-    unsigned bpp = image_in.getBitsPerPixel();
-
+    unsigned bpp = this->image.getBitsPerPixel();
     // Average pooling (applies blur)
-    if (!this->average_pooling(image_in, 7)) {
+    if (!this->average_pooling(this->image, 7)) {
         cerr << "Invalid pooling k" << endl;
         return false;
     }
     
     // Pooling
-    vector<vector<char>> m = this->map_image(image_in);
+    vector<vector<int>> m = this->map_image(this->image);
     if (!this->pooling(m, 7)) {
         cerr << "Invalid pooling k" << endl;
         return false;
     }
-
+    
     // Generation and writing of the image
     fipImage image_out = this->generate_image(m, bpp);
     image_out.save(FIF_PNG, this->out.c_str());
     return true;
 }
 
-char Converter::convert_color(RGBQUAD& color) {
-    unsigned rthreshold = 127;
-    unsigned gthreshold = 127;
-    unsigned bthreshold = 127;
-    char colors[] = {'K', 'B', 'G', 'C', 'R', 'M', 'Y', 'W'};
-    unsigned index = 0;
-    if (color.rgbRed > rthreshold)
-        index += 4;
-    if (color.rgbGreen > gthreshold)
-        index += 2;
-    if (color.rgbBlue > bthreshold)
-        index += 1;
-    return colors[index];
+double Converter::distance(RGBQUAD& color, int cluster) {
+    RGBQUAD cluster_color = this->clusters[cluster];
+    double red = (cluster_color.rgbRed - color.rgbRed) * (cluster_color.rgbRed - color.rgbRed);
+    double green = (cluster_color.rgbGreen - color.rgbGreen) * (cluster_color.rgbGreen - color.rgbGreen);
+    double blue = (cluster_color.rgbBlue - color.rgbBlue) * (cluster_color.rgbBlue - color.rgbBlue);
+    return sqrt(red + green + blue);
 }
 
-RGBQUAD Converter::convert_code(char c) {
-    char colors[] = {'K', 'B', 'G', 'C', 'R', 'M', 'Y', 'W'};
-    unsigned index = 0;
-    RGBQUAD color;
-    while (index < 8 && colors[index] != c)
-        index++;
-    if (index == 8)
-        return color;
-    color.rgbRed = index < 4 ? 0 : 255;
-    color.rgbGreen = (index % 4) < 2 ? 0 : 255;
-    color.rgbBlue = index % 2 == 0 ? 0 : 255;
-    return color; 
+int Converter::get_cluster(RGBQUAD& color) {
+    int cluster = 0;
+    int distance = this->distance(color, 0);
+    for (unsigned i = 1; i < this->clusters.size(); ++i) {
+        double dd = this->distance(color, i);
+        if (dd < distance) {
+            distance = dd;
+            cluster = i;
+        }
+    }
+    return cluster;
 }
 
-vector<vector<char>> Converter::map_image(const fipImage& image) {
+vector<vector<int>> Converter::map_image(const fipImage& image) {
     unsigned width = image.getWidth();
     unsigned height = image.getHeight();
-    vector<vector<char>> m = vector<vector<char>>(width, vector<char>(height));
+    vector<vector<int>> m = vector<vector<int>>(width, vector<int>(height));
     for (unsigned i = 0; i < width; ++i) {
         for (unsigned j = 0; j < height; ++j) {
             RGBQUAD color;
             image.getPixelColor(i, j, &color);
-            m[i][j] = this->convert_color(color);
+            m[i][j] = this->get_cluster(color);
         }
     }
     return m;
 }
 
-fipImage Converter::generate_image(const vector<vector<char>>& m, unsigned bpp) {
+fipImage Converter::generate_image(const vector<vector<int>>& m, unsigned bpp) {
     unsigned width = m.size();
     unsigned height = m[0].size();
     fipImage ret = fipImage(FIT_BITMAP, width, height, bpp);
     for (unsigned i = 0; i < width; ++i) {
         for (unsigned j = 0; j < height; ++j) {
-            RGBQUAD color = this->convert_code(m[i][j]);
+            RGBQUAD color = this->clusters[m[i][j]];
             ret.setPixelColor(i, j, &color);
         }
     }
@@ -91,16 +119,15 @@ fipImage Converter::generate_image(const vector<vector<char>>& m, unsigned bpp) 
     return ret;
 }
 
-bool Converter::pooling(vector<vector<char>>& m, int k) {
+bool Converter::pooling(vector<vector<int>>& m, int k) {
     int width = m.size();
     int height = m[0].size();
-    vector<vector<char>> ret = vector<vector<char>>(width, vector<char>(height));
+    vector<vector<int>> ret = vector<vector<int>>(width, vector<int>(height));
     if (k % 2 == 0 || k < 0)
         return false;
     for (int i = 0; i < width; ++i) {
         for (int j = 0; j < height; ++j) {
-            map<char, int> keys = {{'K', 0}, {'B', 0}, {'G', 0}, {'C', 0},
-                                   {'R', 0}, {'M', 0}, {'Y', 0}, {'W', 0}};
+            vector<int> keys = vector<int>(this->clusters.size(), 0);
             for (int x = -k/2; x <= k/2; ++x) {
                 for (int y = -k/2; y <= k/2; ++y){
                     if (i + x >= 0 && i + x < width && j + y >= 0 && j + y < height){
@@ -108,15 +135,15 @@ bool Converter::pooling(vector<vector<char>>& m, int k) {
                     }
                 }
             }
-            char mcolor = 0;
-            int marg = 0;
-            for (auto it = keys.begin(); it != keys.end(); ++it) {
-                if ((*it).second > marg) {
-                    marg = (*it).second;
-                    mcolor = (*it).first;
+            int cluster = 0;
+            int bcluster  = 0;
+            for (unsigned i = 0; i < keys.size(); ++i) {
+                if (keys[i] > bcluster) {
+                    cluster = i;
+                    bcluster = keys[i];
                 }
             }
-            ret[i][j] = mcolor;
+            ret[i][j] = cluster;
         }
     }
     m = ret;
@@ -155,4 +182,11 @@ bool Converter::average_pooling(fipImage& image, int k) {
     }
     image = ret;
     return true;
+}
+
+void Converter::print_clusters() {
+    for (unsigned i = 0; i < this->clusters.size(); ++i) {
+        cout << "Cluster " << i << " R = " << (int)this->clusters[i].rgbRed << " G = ";
+        cout << (int)this->clusters[i].rgbGreen << " B = " << (int)this->clusters[i].rgbGreen << endl;
+    }
 }
--- src/converter/converter.hh
+++ src/converter/converter.hh
@@ -1,5 +1,6 @@
 #pragma once
 #include <FreeImagePlus.h>
+#include <cmath>
 #include <string>
 #include <iostream>
 #include <vector>
@@ -11,15 +12,21 @@ class Converter {
 
 public:
     Converter(string path_in, string path_out);
-    bool convert();
-    char convert_color(RGBQUAD& color);
-    vector<vector<char>> map_image(const fipImage& image);
-    fipImage generate_image(const vector<vector<char>>& m, unsigned bpp);
-    RGBQUAD convert_code(char c);
-    bool pooling(vector<vector<char>>& m, int k);
-    bool average_pooling(fipImage& image, int k);
+
+    bool convert(); // converts the image
+    double distance(RGBQUAD& color, int cluster); // get cluster distance
+    int get_cluster(RGBQUAD& color); // gets the cluster of a color
+
+    vector<vector<int>> map_image(const fipImage& image); // maps a image into colors
+    fipImage generate_image(const vector<vector<int>>& m, unsigned bpp); //generates a image from a mapping
+
+    bool pooling(vector<vector<int>>& m, int k); // set all pixels to the majority of what's around them
+    bool average_pooling(fipImage& image, int k); // average pooling of image (blurring)
+
+    void print_clusters();
 
 private:
-    string in;
     string out;
+    fipImage image;
+    vector<RGBQUAD> clusters;
 };
